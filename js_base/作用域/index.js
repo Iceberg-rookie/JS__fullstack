1fd038e2f10b9283eac7bb4fce5e415ff84fbd9c
Iceberg-rookie <1318656554@qq.com>
2020-10-30 16:59:28 +0800

    10-30

---
+++ js_base/作用域/index.js
@@ -0,0 +1,50 @@
+// function doSomething(a) {
+  
+//   function doSomethingElse(c) {
+//     return c - 1
+//   }
+
+//   b = a + doSomethingElse(a * 2)
+//   var b
+//   console.log(b * 3);
+// }
+
+// doSomething(2)
+
+// var a = 2;
+// (function () {
+//   var a = 3
+//   console.log(a);
+// })()
+
+// console.log(a);
+
+
+// function a() { // 声明了一个具名函数
+
+// }
+
+// let b = function () { // 声明了一个匿名函数
+
+// }
+
+
+// 块级作用域
+// for(let i = 0; i < 10; i++) {
+  // console.log(i);
+// }
+// console.log(i);
+
+// var a = 1
+// var a; 变量声明 声明提升
+// a = 1
+
+// let a = 2
+// a = 3
+// console.log(a);
+
+// var foo = true
+// if (foo) {
+  // var a = 2
+// }
+// console.log(a);
