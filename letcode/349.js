2175953f1f369ab2a25daf4215332cc1c66c0b37
Iceberg-rookie <1318656554@qq.com>
2020-11-02 20:00:30 +0800

    11-02

---
+++ letcode/349.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+  var num = []
+  var tmp = []
+  for(var i = 0; i < nums1.length; i++) {
+    for(var j = 0; j < nums2.length; j++){
+      if(nums1[i] == nums2[j]) {
+        tmp.push(nums1[i])
+      }
+    }
+  }
+  for(var k in tmp) {
+    if(num.indexOf(tmp[k]) == -1) {
+      num.push(tmp[k])
+    }
+  }
+  // console.log(num);
+  return num
+};
+
+// intersection([1,2,2,1],[2,2])
+// intersection([4,9,5],[9,4,9,8,4])
