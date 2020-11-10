85ebb9c094d40152b056e5aa810f8320aca3b1c3
Iceberg-rookie <1318656554@qq.com>
2020-11-10 12:38:52 +0800

    2020-11-10

---
+++ es6/解构/index.js
@@ -0,0 +1,37 @@
+// let a = 1
+// let b = 2
+// let c = 3
+
+// let [b, [a, c]] = [1, [2, 3]]
+// console.log(a);
+// ...arguments
+
+// let [a, b, ...c] = [1]
+
+// let [a] = false
+
+
+// console.log(a);
+
+
+// set
+// let arr = [1, 2, 2, 4, 6, 3, 5, 4]
+// let newArr = new Set(arr)
+// console.log(newArr);
+
+// let [x, y, z] = {a: 'a', b: 'b', c: 'c'}
+// console.log(x);
+
+// let [x, y, z] = new Set(['a', 'b', 'c'])
+// console.log(x);
+
+// let [foo = true] = [false]
+// console.log(foo);
+
+// let [x, y = 'b'] = ['a', null]
+// console.log(y);
+
+// let {y, x, z} = {x: 'aaa', y: 'bbb'}
+// console.log(z);
+
+let { log, sin, cos } = Math
