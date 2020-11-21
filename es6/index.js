cba1d4e44857b2bc1b304166aa0b7e28cb35f948
Iceberg-rookie <1318656554@qq.com>
2020-11-21 17:41:21 +0800

    es6

--- es6/index.js
+++ es6/index.js
@@ -11,3 +11,43 @@
 // const foo = 'this is' + example
 // const foo = `this is ${example}`
 
+// es6 新增的数组的方法
+let arr = ['a', 'b', 'c']
+// console.log(Object.keys(arr));
+// let newArr = [...arr.keys()]
+// console.log(newArr);
+
+// let iter = arr.entries()
+// for(let e of iter) {
+//   console.log(e);
+// }
+// console.log(iter);
+
+// let eArr = arr.values()
+// for (let e of eArr) {
+//   console.log(e);
+// }
+
+// function test(fruit) {
+//   const redFruits = ['apple', 'strawberry', 'cheery']
+//   if(redFruits.includes(fruit)) {
+//     console.log('red');
+//   }
+// }
+// test('cheery')
+
+// let people = [
+//   { name: '小陶', age: 20 },
+//   { name: '叶总', age: 18 },
+//   { name: '大陶', age: 21 }
+// ]
+// function findFriend(person) {
+//   return person.name === '叶总'
+// }
+// console.log(people.find(findFriend));
+
+let test = ['a', 'b', 'c', 'd']
+function index(i) {
+  return i == 'b'
+}
+console.log(test.findIndex(index));
