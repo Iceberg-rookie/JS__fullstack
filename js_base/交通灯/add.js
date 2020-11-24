e3d58093dea2831e4ceda6535830d55e671eefc3
Iceberg-rookie <1318656554@qq.com>
2020-11-24 10:01:20 +0800

    add

--- js_base/交通灯/add.js
+++ js_base/交通灯/add.js
@@ -5,13 +5,17 @@
 // 9
 function add(num1, num2) {
   let carry = 0, res = ''
-  for(let i = num1.length, j = num2.length;
+  for(let i = num1.length - 1, j = num2.length - 1;
     i >= 0 || j >= 0; i--, j--) {
       let a = num1.charAt(i) || 0
       let b = num2.charAt(j) || 0
-      let sum = a + b + carry
+      let sum = Number(a) + Number(b) + carry
       sum >= 10 ? carry = 1 : carry = 0
-      res = sum + res
+      res = (sum % 10) + res
   }
+  if(carry === 1) {
+    res = 1 + res
+  }
+  return res
 }
-add('99', '9')
+console.log(add('99', '9'));
