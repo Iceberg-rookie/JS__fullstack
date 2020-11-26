f5360d8708c8ac88e2d9acdc084a4e46b18cff5f
Iceberg-rookie <1318656554@qq.com>
2020-11-26 18:13:45 +0800

    new

--- js_base/手写系列/new/index.js
+++ js_base/手写系列/new/index.js
@@ -1,7 +1,20 @@
+Person.prototype.say = function() {
+  console.log('hahaha');
+}
 function Person (name, age) {
   this.name = name
   this.age = age
 }
 
 let person = new Person('小敏', '18')
-console.log(person);
+// console.log(person);
+person.say()
+
+// function myNew() {
+//   let obj = {}
+
+//   return obj
+// }
+
+// let person = myNew(Person, '小敏')
+// console.log(person);
