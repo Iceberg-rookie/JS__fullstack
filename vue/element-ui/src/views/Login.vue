c9f6f9537794ded8ad52782280d35cde2bc55375
Iceberg-rookie <1318656554@qq.com>
2020-12-08 22:31:23 +0800

    element-ui

---
+++ vue/element-ui/src/views/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="login">
+    <div class="login-container">
+      <h2>旅梦后台管理系统</h2>
+      <div class="wrap">
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+.login{
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-container{
+  width: 300px;
+  height: 260px;
+  box-shadow: 0px 0px 5px #999;
+  border-radius: 5px;
+  text-align: center;
+}
+</style>
