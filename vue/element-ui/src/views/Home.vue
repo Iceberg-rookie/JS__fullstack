7775d91383632e60dd6644aedf8761a6745e949a
Iceberg-rookie <1318656554@qq.com>
2020-12-09 14:15:36 +0800

    element-ui

--- vue/element-ui/src/views/Home.vue
+++ vue/element-ui/src/views/Home.vue
@@ -3,10 +3,48 @@
     <el-container>
       <el-header>
         <div class="title">旅梦管理系统</div>
-        <div class="info">{{userName}}</div>
+        <div class="info">
+          <el-dropdown @command="handleCommand">
+            <span class="el-dropdown-link">
+              {{ userName }}<i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item command="1">修改密码</el-dropdown-item>
+              <el-dropdown-item command="2">退出登录</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
       </el-header>
       <el-container>
-        <el-aside width="200px">Aside</el-aside>
+        <el-aside width="200px">
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+          >
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>学生列表</span>
+              </template>
+              <el-menu-item>
+                <span slot="title">酒神</span>
+              </el-menu-item>
+              <el-menu-item>
+                <span slot="title">小敏</span>
+              </el-menu-item>
+              <el-menu-item>
+                <span slot="title">卢瑞祯</span>
+              </el-menu-item>
+            </el-submenu>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">作业情况</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
         <el-main>Main</el-main>
       </el-container>
     </el-container>
@@ -17,47 +55,70 @@
 export default {
   data() {
     return {
-      userName: ''
-    }
+      userName: "",
+    };
   },
   created() {
-    this.userName = localStorage.getItem('user')
-  }
+    this.userName = localStorage.getItem("user");
+  },
+  methods: {
+    handleCommand(e) {
+      // console.log(e);
+      if (e == 2) {
+        this.$router.push("/");
+        localStorage.removeItem("user");
+      }
+    },
+  },
 };
 </script>
   
 <style>
-  .el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-  }
-  
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-  }
-  
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-  }
-  
-  body > .el-container {
-    margin-bottom: 40px;
-  }
-  .el-container {
-    height: calc(100vh - 60px);
-  }
-  .title {
-    float: left;
-    font-weight: 700;
-    font-size: 24px;
-  }
-  .info {
-    float: right;
-  }
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+.el-container {
+  height: calc(100vh - 60px);
+}
+.title {
+  float: left;
+  font-weight: 700;
+  font-size: 24px;
+}
+.info {
+  float: right;
+}
+.el-dropdown-link {
+  cursor: pointer;
+  color: #409eff;
+}
+.el-icon-arrow-down {
+  font-size: 12px;
+}
+.el-menu {
+  height: 100%;
+}
+.el-submenu .el-menu-item {
+  min-width: auto !important;
+}
 </style>
