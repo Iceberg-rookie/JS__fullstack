7cca4d8591b5e09498327a1848ede8939cfa17c4
Iceberg-rookie <1318656554@qq.com>
2020-12-14 11:54:53 +0800

    vue-weather

--- vue/vue-weather/src/views/Index.vue
+++ vue/vue-weather/src/views/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="nav">
-      <div class="time">{{localTime}}</div>
+      <div class="time">{{ localTime }}</div>
       <div class="city">切换城市</div>
     </div>
 
@@ -10,36 +10,66 @@
       <p class="weather">阴</p>
       <p class="temp"><em>9</em>℃</p>
       <div class="detail">
-        <span>风力:3</span> | <span>风向：西北</span> | <span>空气湿度：75%</span>
+        <span>风力:3</span> | <span>风向：西北</span> |
+        <span>空气湿度：75%</span>
       </div>
     </div>
+
+    <div class="map-container" ref="mapContainer"></div>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      localTime: ''
-    }
+      localTime: "",
+    };
   },
-  created () {
+  created() {
     setInterval(() => {
-      this.localTime = this.getLocalTime()
-    }, 1000)
+      this.localTime = this.getLocalTime();
+    }, 1000);
+  },
+  mounted() {
+    this.initMap();
   },
   methods: {
-    getLocalTime () {
-      return new Date().toLocaleTimeString()
+    getLocalTime() {
+      return new Date().toLocaleTimeString();
     },
-    initMap () { // 获取当前城市
+    initMap() {
+      // 获取当前城市
+      let _this = this;
+      var map = new AMap.Map(_this.$refs.mapContainer, {
+        resizeEnable: true,
+      });
+      AMap.plugin("AMap.CitySearch", function () {
+        var citySearch = new AMap.CitySearch();
+        citySearch.getLocalCity(function (status, result) {
+          if (status === "complete" && result.info === "OK") {
+            // 查询成功，result即为当前所在城市信息
+            // console.log(result);
+            _this.getCurrentCityData(result.city);
+          }
+        });
+      });
+    },
+    getCurrentCityData(cityName) {
+      // 查询天气
+      //加载天气查询插件
+      AMap.plugin("AMap.Weather", function () {
+        //创建天气查询实例
+        var weather = new AMap.Weather();
 
+        //执行实时天气信息查询
+        weather.getLive(cityName, function (err, data) {
+          console.log(err, data);
+        });
+      });
     },
-    getCurrentCityData () { // 查询天气
-      
-    }
-  }
-}
+  },
+};
 </script>
 
 <style lang="less">
@@ -59,8 +89,11 @@ export default {
       em {
         font-size: 34px;
         font-style: normal;
-    }
+      }
     }
   }
+}
+.map-container {
+  height: 300px;
 }
 </style>
