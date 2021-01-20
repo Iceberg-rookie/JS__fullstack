476346df5f00ebc724813ee1a29fd7c687065bbd
Iceberg-rookie <1318656554@qq.com>
2021-01-20 10:11:18 +0800

    vue-elm

--- vue/vue-elm/src/App.vue
+++ vue/vue-elm/src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <div id="app">
-    
-    <router-view/>
+  <div class="app">
+    <Header></Header>
   </div>
 </template>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+<script>
+import Header from '@/components/header/Header'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+
 </style>
