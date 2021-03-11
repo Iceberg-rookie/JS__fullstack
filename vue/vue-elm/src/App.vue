97e042841bc69aca068780e1d4f2d04808ef8590
Iceberg-rookie <1318656554@qq.com>
2021-03-11 18:58:42 +0800

    vue-elm

--- vue/vue-elm/src/App.vue
+++ vue/vue-elm/src/App.vue
@@ -6,9 +6,23 @@
 
 <script>
 import Header from '@/components/header/Header'
+import { getSeller } from '@/api'
 export default {
+  data () {return {
+    seller: {
+      id: 1
+    }
+    }
+  },
   components: {
     Header
+  },
+  created() {
+    getSeller({
+      id: this.seller.id
+    }).then((seller) => {
+      console.log(seller);
+    })
   }
 }
 </script>
