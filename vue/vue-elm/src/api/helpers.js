7cf133ffb0b8ea757a82d3410b6486ba55c5281d
Iceberg-rookie <1318656554@qq.com>
2021-03-11 16:49:03 +0800

    vue-elm

--- vue/vue-elm/src/api/helpers.js
+++ vue/vue-elm/src/api/helpers.js
@@ -1,9 +1,18 @@
 import axios from 'axios'
 
-const baseUrl = 'www.baidu.com';
+const baseUrl = '';
 
 export function get(url) {
   return function(params = {}) {
-    return axios.get(baseUrl + url)
+    return axios.get(baseUrl + url, {
+      params
+    }).then((res) => {
+      const { errno, data } = res.data
+      if(errno === 0) {
+        return data
+      }
+    }).catch((err) => {
+      console.log(err);
+    })
   }
 }
