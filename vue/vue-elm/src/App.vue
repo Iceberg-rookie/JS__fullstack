ca7214932cd3b92df02cec4ff414dbb4240004c3
Iceberg-rookie <1318656554@qq.com>
2021-03-15 11:31:09 +0800

    vue-elm

--- vue/vue-elm/src/App.vue
+++ vue/vue-elm/src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="app">
     <Header :seller="seller"></Header>
+    <div class="tab">
+      <div class="tab-wrapper">
+        <router-link to="/">商品</router-link>
+      </div>
+      <div class="tab-wrapper">
+        <router-link to="/comment">评论</router-link>
+      </div>
+      <div class="tab-wrapper">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -32,5 +43,20 @@ export default {
 </script>
 
 <style lang="stylus">
-
+@import './common/stylus/variable.styl'; 
+.tab
+  width 100%
+  display flex
+  height 36px
+  &-wrapper
+    flex 1
+    text-align center
+    a
+      width 100%
+      display inline-block
+      color #666
+      text-decoration none
+    .router-link-exact-active
+      color $color-red
+      border-bottom 2px solid $color-red
 </style>
