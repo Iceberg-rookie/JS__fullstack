805148deb493bd23ae3df2308e13e880887e7726
Iceberg-rookie <1318656554@qq.com>
2021-03-22 13:25:14 +0800

    vuex-demo

--- vue/vuex/src/components/count.vue
+++ vue/vuex/src/components/count.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="count">
     <h3>{{$store.state.count}}</h3>
+    <h2>{{count}}</h2>
+    <h1>{{msg}}</h1>
     <p>
       <button @click="$store.commit('add')">add</button>
       <button @click="$store.commit('reduce')">reduce</button>
@@ -9,8 +11,20 @@
 </template>
 
 <script>
-export default {
+import { mapState } from 'vuex'
 
+export default {
+  // computed: {
+    // newCount() {
+      // return this.$store.state.count*2
+    // }
+  // }
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  computed: mapState(['count'])
 }
 </script>
 
