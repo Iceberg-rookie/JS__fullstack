2821e6f5bd3d33aaf796fa67a8b3130500fe158a
Iceberg-rookie <1318656554@qq.com>
2021-04-03 22:34:28 +0800

    node ai

---
+++ ai/detect_car/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const AipImageClassifyClinet = 
+  require('baidu-aip-sdk').imageClassify
+
+const APP_ID = "23927521";
+const API_KEY = "R4aIyG9MnnU9PLQxBaGN9b2R";
+const SECRET_KEY = "UG1b8bZZpzXAlHNAs0CqKtSVLh5oG1nV";
+
+const image = fs.readFileSync()
