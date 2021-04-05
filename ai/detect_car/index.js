dc0ad7633d2962310a487ce39283043014c6e2aa
Iceberg-rookie <1318656554@qq.com>
2021-04-05 18:03:58 +0800

    ai

--- ai/detect_car/index.js
+++ ai/detect_car/index.js
@@ -6,5 +6,20 @@ const AipImageClassifyClinet =
 const APP_ID = "23927521";
 const API_KEY = "R4aIyG9MnnU9PLQxBaGN9b2R";
 const SECRET_KEY = "UG1b8bZZpzXAlHNAs0CqKtSVLh5oG1nV";
+// sync I/O Async 同步
+const image = fs.readFileSync("car.jpg")
+  .toString("base64")
+// console.log(image)
 
-const image = fs.readFileSync()
+const client = new AipImageClassifyClinet(APP_ID,
+  API_KEY, SECRET_KEY);
+
+client
+  // http 协议
+  // 云端 AI sdk
+  .carDetect(image)
+  .then((result) => {
+    console.log(result);
+  }).catch(err => {
+    console.log(err);
+  })
