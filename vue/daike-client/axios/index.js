7fdbdc63aae6ee6bf082c1cce2942353b26527d5
Iceberg-rookie <1318656554@qq.com>
2021-04-26 12:59:33 +0800

    daike-client

---
+++ vue/daike-client/axios/index.js
@@ -0,0 +1,18 @@
+import { defineProperties } from 'core-js/core/object'
+import apiLogin from './interface/login'
+
+const install = Vue => {
+  Object.defineProperties(Vue.prototype, {
+    $http: {
+      get() {
+        return Object.assign(
+          {},
+          apiLogin,
+          // ...
+        )
+      }
+    }
+  })
+}
+
+export default install
