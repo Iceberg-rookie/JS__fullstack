7329df51cb270f5662d67d0ac2f3db56e97f8fd7
Iceberg-rookie <1318656554@qq.com>
2021-04-27 20:05:39 +0800

    daike

--- vue/daike-client/src/views/login/index.vue
+++ vue/daike-client/src/views/login/index.vue
@@ -3,9 +3,23 @@
     <img class="logo" src="@/assets/daike.png" alt="">
   
     <van-cell-group class="box">
-      <van-field v-model="username" label="用户名" placeholder="请输入用户名" />
-      <van-field v-model="password" label="密码" placeholder="请输入密码" />
+      <van-field
+        v-model="username"
+        label="用户名"
+        placeholder="请输入用户名" />
+
+      <van-field
+        v-model="password"
+        label="密码"
+        placeholder="请输入密码"
+        type="password" />
+
     </van-cell-group>
+
+    <van-row>
+      <van-button size="small" type="default">注册</van-button>
+      <van-button size="small" type="primary" class="btn-login" @click="handleLogin">登录</van-button>
+    </van-row>
   </div>
 </template>
 
@@ -13,7 +27,35 @@
 export default {
   data() {
     return {
-      username: ""
+      username: "",
+      password: "",
+    }
+  },
+  methods: {
+    showLoginTip(status) {
+      this.$toast.loading({
+        message: status,
+        forbidClick: true,
+        loadingType: 'spinner',
+        duration: 0
+      })
+    },
+    login(){
+      this.$http.login({
+        username: this.username,
+        password: this.password
+      }).then(res => {
+        console.log(res);
+      })
+    },
+    handleLogin() {
+      if(this.username.trim() == '' || this.password.trim() == '') {
+        this.$toast.fail('用户名或密码不能为空')
+        return
+      }
+
+      this.showLoginTip('登录中...')
+      this.login()
     }
   }
 }
