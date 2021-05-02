9714daedcd8a39dfb256a4ccaf42be13e2d1ac30
Iceberg-rookie <1318656554@qq.com>
2021-05-02 23:42:48 +0800

    数组去重

--- js_base/手写系列/数组去重/unique.js
+++ js_base/手写系列/数组去重/unique.js
@@ -1,20 +1,45 @@
 // _.uniq(array, [isSorted], [iteratee])去重
 
 let array1 = [1, 2, '1', 2, 1]
-let array2 = [1, 1, '1', 2, 2]
+let array2 = [1, 1, '1', 2, 2, 'a', 'b', 'A']
 
-function unique(array, isSorted) {
+// function unique(array, isSorted) {
+//   let res = []
+//   let seen
+
+//   for (let i = 0, len = array.length; i < len; i++) {
+//     let value  = array[i]
+//     if (isSorted) {
+//       if (!i || seen !== value) {
+//         res.push(value)
+//       }
+//       seen = value
+//     } else if (res.indexOf(value) === -1) {
+//       res.push(value)
+//     }
+//   }
+//   return res
+// }
+
+function unique(array, isSorted, interatee) {
   let res = []
-  let seen
+  let seen = []
 
   for (let i = 0, len = array.length; i < len; i++) {
     let value  = array[i]
+    let computed = interatee ? interatee(value, i, array) : value
     if (isSorted) {
-      if (!i || seen !== value) {
+      if (!i || seen !== computed) {
         res.push(value)
       }
-      seen = value
-    } else if (res.indexOf(value) === -1) {
+      seen = computed
+    } else if (interatee) {
+      if (seen.indexOf(computed) === -1) {
+        seen.push(computed)
+        res.push(value)
+      }
+    }
+    else if (res.indexOf(value) === -1) {
       res.push(value)
     }
   }
@@ -22,4 +47,6 @@ function unique(array, isSorted) {
 }
 
 console.log(unique(array1));
-console.log(unique(array2));
+console.log(unique(array2, false, function(item) {
+  return typeof item == 'string' ? item.toLowerCase() : item
+}));
