7d6e6086f69966e2c07cd12d7cfb2cc0cadf9f26
Iceberg-rookie <1318656554@qq.com>
2021-05-03 22:52:19 +0800

    深浅拷贝

--- js_base/手写系列/深浅拷贝/index.js
+++ js_base/手写系列/深浅拷贝/index.js
@@ -3,9 +3,15 @@
 
 // JS中，在栈里存储数据的赋值实现的是深拷贝的效果，在堆里存储数据的赋值实现的是浅拷贝的效果
 
-let arr = [{old: 'old'}, 1, true, null, undefined]
-let new_arr = arr.concat()
-arr[0].old = 'new'
+// let arr = [{old: 'old'}, 1, true, null, undefined]
+// let new_arr = arr.concat()
+// arr[0].old = 'new'
 
-console.log(arr);
-console.log(new_arr);
+// console.log(arr);
+// console.log(new_arr);
+
+// let arr = [{old: 'old'} , 1, true, null, undefined]
+// let new_arr = arr.slice()
+// arr[0].old = 'new'
+// console.log(new_arr);
+
