2415fa6a456188779b71cd76d4578a66c389a8e8
Iceberg-rookie <1318656554@qq.com>
2021-05-03 22:45:43 +0800

    数组去重

--- js_base/手写系列/深浅拷贝/index.js
+++ js_base/手写系列/深浅拷贝/index.js
@@ -1,8 +1,11 @@
+// 深拷贝不受原数据的影响。如同克隆人
+// 浅拷贝会受原数据的影响。如同影子
+
 // JS中，在栈里存储数据的赋值实现的是深拷贝的效果，在堆里存储数据的赋值实现的是浅拷贝的效果
 
-let arr = ['old', 1, true, null, undefined]
+let arr = [{old: 'old'}, 1, true, null, undefined]
 let new_arr = arr.concat()
-arr[0] = 'new'
+arr[0].old = 'new'
 
 console.log(arr);
 console.log(new_arr);
