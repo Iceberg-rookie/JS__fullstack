5895f44bae725f57b35e2d2e7813872990830e1d
Iceberg-rookie <1318656554@qq.com>
2021-05-04 23:41:40 +0800

    深浅拷贝

--- js_base/手写系列/深浅拷贝/shallow.js
+++ js_base/手写系列/深浅拷贝/shallow.js
@@ -4,7 +4,7 @@ let shallowCopy = function(obj) {
   // 根据obj的类型判断是新建一个数组还是对象
   let newObj = obj instanceof Array ? [] : {}
   for (let key in obj) {
-    if(obj.hasOwnProperty(key)) {
+    if(obj.hasOwnproperty(key)) {
       newObj[key] = obj[key]
     }
   }
