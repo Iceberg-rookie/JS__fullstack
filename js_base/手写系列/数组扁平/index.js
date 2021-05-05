41289bf9a93fcd57474725af82306823c234e976
Iceberg-rookie <1318656554@qq.com>
2021-05-05 16:26:05 +0800

    数组扁平

--- js_base/手写系列/数组扁平/index.js
+++ js_base/手写系列/数组扁平/index.js
@@ -1,16 +1,25 @@
 let arr = [1, [2, [3, 4]]]
 
-function falltten(arr) {
-  let result = []
-  for (let i = 0, len = arr.length; i < len; i++) {
-    if (Array.isArray(arr[i])) {
-      result = result.concat(falltten(arr[i]))
-    } else {
-      result.push(arr[i])
-    }
-  }
-  return result
-}
+// function falltten(arr) {
+//   let result = []
+//   for (let i = 0, len = arr.length; i < len; i++) {
+//     if (Array.isArray(arr[i])) {
+//       result = result.concat(falltten(arr[i]))
+//     } else {
+//       result.push(arr[i])
+//     }
+//   }
+//   return result
+// }
+// console.log(falltten(arr)); // [1, 2, 3, 4]
 
+// console.log(arr.toString(arr));
 
-console.log(falltten(arr)); // [1, 2, 3, 4]
+// reduce
+// function falltten(arr) {
+//   return arr.reduce(function (prev, next) {
+//     return prev.concat(Array.isArray(next) ? falltten(next) : next)
+//   }, [])
+// }
+
+console.log([].concat(...arr));
