0f2ba7ae441a0cfe843924504d5b3391a24a948f
Iceberg-rookie <1318656554@qq.com>
2021-05-05 13:47:34 +0800

    深浅拷贝

---
+++ js_base/手写系列/数组最大-小值/index.js
@@ -0,0 +1,36 @@
+// let array = [1, 2, 3, 0, 5, '6', undefined]
+// console.log(Math.max(...array));
+// console.log(Math.min(...array));
+
+// console.log(Math.max(true, '2', null));
+// console.log(Math.min(1, '0', {}));
+
+// console.log(Math.min() > Math.max()); // true
+// console.log();
+
+//原始方法
+let array = [1, 2, 3, 2, 0, 5, 8]
+// function max(arr) {
+//   let max = arr[0]
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i]
+//     }
+//   }
+//   return max
+// }
+
+// reduce
+function max(prev, next) {
+  return Math.max(prev, next)
+}
+
+console.log(arr.reduce(max));
+
+
+// 排序
+array.sort(function (a, b) {
+  return b - a
+})
+
+console.log(array[0]);
